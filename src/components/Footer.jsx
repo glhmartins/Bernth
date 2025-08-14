b20e6cbd28aa20899ab1e087575f3cc8dec42072
glhmartins <glhmartinsrangel@gmail.com>
2025-08-14 07:40:29 -0300

    footer desktop completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,44 +1,44 @@
-import style from './css/Footer.module.css'
-import {Link} from 'react-router-dom'
-import SweatRecords from '../assets/Sweat-Records.png'
+import style from './css/Footer.module.css';
+import {Link} from 'react-router-dom';
+import SweatRecords from '../assets/Sweat-Records.png';
+import {FaYoutube, FaInstagram, FaFacebook} from 'react-icons/fa6';
+import {FaPatreon} from 'react-icons/fa';
 
 function Footer(){
     return(
-        <>
-            <footer className={style.footer}>
+        <footer>
+            <div className={style.footer}>
             
                 {/* Sweat Records logo */}
                 <img src={SweatRecords} className={style.img} alt="logo sweat records"/>
                 
                 {/* Social Medias */}
-                <div className={style.social}>
-                    <Link to="http://www.facebook.com/bernthofficial"></Link>
-                    <Link to="http://www.instagram.com/bernthofficial"></Link>
-                    <Link to="http://www.youtube.com/bernthguitar"></Link>
-                    <Link to="https://www.patreon.com/bernth"></Link>
+                <div className={style.socials}>
+                    <Link className={style.social} to="http://www.facebook.com/bernthofficial"><FaFacebook/></Link>
+                    <Link className={style.social} to="http://www.instagram.com/bernthofficial"><FaInstagram/></Link>
+                    <Link className={style.social} to="http://www.youtube.com/bernthguitar"><FaYoutube/></Link>
+                    <Link className={style.social} to="https://www.patreon.com/bernth"><FaPatreon/></Link>
                 </div>
 
                 {/* Links */}
-                <div classNames={style.page}>
-                    <Link to="/Terms-And-Service">Terms and Service</Link>
-                    <Link to="/Imprint">Imprint</Link>
-                    <Link to="/Legal-Disclosure">Legal Disclosure</Link>
-                    <Link to="/Privacy-Policy">Privacy Policy</Link>
+                <div classNames={style.pages}>
+                    <Link className={style.page} to="/Terms-And-Service">Terms and Service</Link>
+                    <Link className={style.page} to="/Imprint">Imprint</Link>
+                    <Link className={style.page} to="/Legal-Disclosure">Legal Disclosure</Link>
+                    <Link className={style.page} to="/Privacy-Policy">Privacy Policy</Link>
                 </div>
 
                 {/* Copyright */}
-                <p>&copy; Online Music Lessons Vienna GenbR 2025</p>
-                
-                {/* Content below the gray part */}
-                <div className={style.Digi}>
-                    <strong>
-                        POWERED BY
-                        <Link to="https://www.digistore24.com/redir/22481/Bernth/d3d3LmJlcm50aC5hdA==" className={style.text}>DIGIMEMBER</Link>
-                    </strong>
-                </div>
+                <p className={style.copyright}>&copy; Online Music Lessons Vienna GenbR 2025</p>
 
-            </footer>
-        </>
+            </div>
+
+            {/* Content below the gray part */}
+                <strong className={style.Digi}>
+                    POWERED BY
+                    <Link to="https://www.digistore24.com/redir/22481/Bernth/d3d3LmJlcm50aC5hdA==" className={style.text}>DIGIMEMBER</Link>
+                </strong>
+        </footer>
     )
 }
 
