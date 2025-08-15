d2b89e3f3e2c1ea476e2b23b074af4dc1b35aa6e
glhmartins <glhmartinsrangel@gmail.com>
2025-08-15 13:37:38 -0300

    Starting Privacy Policy

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -1,6 +1,51 @@
+import style from './css/PrivacyPolicy.module.css'
+
 function PrivacyPolicy(){
     return(
-        <></>
+        <div className={style.privacyPolicy}>
+            <h1>Privacy policy</h1>
+            
+            <section>
+                <p>Bernth.at is serious about protecting your online privacy. This Privacy Statement explains our views and practices concerning privacy, and how they may pertain to you as a user of our website.</p>
+                <br/>
+                <p>You or Your means you as a participant in or as a user of the Bernth.at website. We or Our or Us means Bernth.at. Our site means Bernth.at.</p>
+                <br/>
+            </section>
+            
+
+            <section>
+                <h2>Section 1 – What we do with your information</h2>
+                <p>When you purchase something from our site, as part of the buying and selling process, we collect the personal information you give us such as your name, address and email address. This enables us to both fulfill and support the products you purchase.</p>
+                <h3>Who is responsible for the data collection on this website?</h3>
+                <p></p>
+            </section>
+
+
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+
+        </div>
     )
 }
 
