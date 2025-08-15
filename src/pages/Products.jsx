1a78ff45201218fc994af16c1584d4e919e42e63
glhmartins <glhmartinsrangel@gmail.com>
2025-08-15 20:34:44 -0300

    Products desktop completed

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -14,9 +14,9 @@ function Products(){
             <div className={style.sweepProduct}>
                 <img src={screenSweep} className={style.sweepScreens} alt="Screens with the course image"/>
                 <div className={style.sweepText}>
-                    <h1>Sweep Picking Masterclass</h1>
-                    <h3>Sweep Picking Masterclass is a 10 week online course and consists of:</h3>
-                    <p>40 in-depth instructional videos (2,5h runtime)<br/>
+                    <h1 className={style.ph1}>Sweep Picking Masterclass</h1>
+                    <h3 className={style.ph3}>Sweep Picking Masterclass is a 10 week online course and consists of:</h3>
+                    <p className={style.pp}><br/>40 in-depth instructional videos (2,5h runtime)<br/>
                         Playalong Bundle: 46 practice playalong videos (5h runtime) - practice every single exercise together with me in different tempos<br/>
                         Sweep Picking Application Bundle: learn how to apply everything we learn about technique and theory in week 9 and 10 with creative practical example compositions<br/>
                         Level up 4 times every week on the Sweep Picking Skill Tree - with this visual element you can track your progress and it makes learning rewarding and fun<br/>
@@ -26,13 +26,12 @@ function Products(){
                         Note overlay graphics - easily memorize and identify the shapes and arpeggios on the fretboard<br/>
                         Audio downloads for every playalong video - take the workouts with you to practice anywhere and anytime without internet connection<br/>
                     </p>
-                    <sub>
+                    <sub className={style.psub}>
                         ** (The course videos can only be watched on the Sweep Picking Masterclass website. Videos cannot be downloaded for offline access.<br/>
                         The Payment will be managed by DigiStore24 GmbH (DigiStore24.com)<br/>
-                        <br/>
-                        <Link to="/Terms-And-Service">Read about the terms of service</Link><br/>
+                        <Link className={style.link} to="/Terms-And-Service">Read about the terms of service</Link><br/>
                     </sub>
-                    <button className={style.btn}>LEARN MORE</button>
+                    <button className={style.btn}><Link className={style.linkBtn} to="https://www.digistore24.com/product/298626">LEARN MORE</Link></button>
                 </div>
             </div>
             <div className={style.stepBanner}>
@@ -41,9 +40,9 @@ function Products(){
             <div className={style.stepProduct}>
                 <img src={screenStep} className={style.stepScreens} alt="Screens with the course image"/>
                 <div className={style.stepText}>
-                    <h1>10 Steps to Modern Shredding</h1>
-                    <h3>10 Steps to Modern Shredding is a 10 week online course and consists of:</h3>
-                    <p>
+                    <h1 className={style.ph1}>10 Steps to Modern Shredding</h1>
+                    <h3 className={style.ph3}>10 Steps to Modern Shredding is a 10 week online course and consists of:</h3>
+                    <p className={style.pp}><br/>
                         40 different tutorial videos<br/>
                         10 steps divided into 4 chapters each, from beginner to advanced<br/>
                         In-depth explanation of sweeping, tapping, fast alternate picking and more<br/>
@@ -53,11 +52,13 @@ function Products(){
                         Practice anywhere, anytime<br/>
                         Improve your technique and theory knowledge to become a modern guitar virtuoso<br/>
                     </p>
-                    <sub>
-                        ** (The course videos can only be watched on the 10 Steps to Modern Shredding website. Videos cannot be downloaded for offline access.
-                        The Payment will be managed by DigiStore24 GmbH (DigiStore24.com)<br/>
+                    <sub className={style.psub}>
+                        ** (The course videos can only be watched on the 10 Steps to Modern Shredding website. Videos cannot be downloaded for offline access.<br/>
+                        The Payment will be managed by DigiStore24 GmbH (DigiStore24.com)
+                        <br/>
+                        <Link className={style.link} to="/Terms-And-Service">Read about the terms of service</Link><br/>
                     </sub>
-                    <button className={style.btn}>LEARN MORE</button>
+                    <button className={style.btn}><Link className={style.linkBtn} to="https://www.bernth.at/10-steps-click/">LEARN MORE</Link></button>
                 </div> 
             </div>
         </div> 
