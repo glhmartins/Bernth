be69efd777a6d6c36093fd8c93b53366a25bbe55
glhmartins <glhmartinsrangel@gmail.com>
2025-08-16 07:27:57 -0300

    Login desktop version

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,23 @@
+import style from './css/Login.module.css'
+
 function Login(){
     return(
-        <></>
+        <div className={style.forms_container}>
+            <div className={style.background}>
+                <form className={style.forms} action="/" method="post">
+                    <div className={style.inputs}>
+                        <label className={style.labelInput}>USERNAME</label><br/>
+                        <input type="text" className={style.inputForms}/><br/>
+                        <label className={style.labelInput}>PASSWORD</label><br/>
+                        <input type="password" className={style.inputForms}/><br/>
+                        <input type="checkbox" className={style.checkBox}/>
+                        <label className={style.labelCheck}>Remember me</label><br/>
+                        <p className={style.lostPassword}>Lost your password?</p>
+                    </div>
+                        <button type="submit" className={style.btn}>LOGIN</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
