c2120be03610cfafbec244d2d0a30a085ae2bd6e
glhmartins <glhmartinsrangel@gmail.com>
2025-08-16 19:53:44 -0300

    active option in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,44 @@
 import Logo from '../assets/logo.png'
 import style from './css/Navbar.module.css'
-import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 
 function Navbar(){
     return(
         <nav className={style.navbar}>
 
             {/* Navbar image */}
-            <Link to="/" className={style.img}>
+            <NavLink to="/" className={style.img}>
                 <img src={Logo} alt="Logo Bernth"/>
-            </Link>
+            </NavLink>
 
             {/* Navbar links */}
             <ul className={style.list}>
                 <li>
-                    <Link to="/" className={style.item}>HOME</Link>
+                    <NavLink to="/"
+                        className={({isActive}) => (isActive ? `${style.item} ${style.active}`: style.item)}>
+                        HOME
+                    </NavLink>
                 </li>
                 <li>
-                    <Link to="/Products" className={style.item}>PRODUCTS</Link>
+                    <NavLink to="/Products" 
+                        className={({isActive})=> (isActive ? `${style.item} ${style.active}` : style.item)}>
+                        PRODUCTS
+                    </NavLink>
                 </li>
                 <li>
-                    <Link to="https://bernth.myshopify.com/" className={style.item}>MERCH</Link>
+                    <a href="https://bernth.myshopify.com/" className={style.item}>MERCH</a>
                 </li>
                 <li>
-                    <Link to="/Terms-And-Service" className={style.item}>TERMS AND SERVICE</Link>
+                    <NavLink to="/Terms-And-Service" 
+                        className={({isActive}) => (isActive ? `${style.item} ${style.active}` : style.item)}>
+                        TERMS AND SERVICE
+                    </NavLink>
                 </li>
                 <li>
-                    <Link to="/Login" className={style.item}>LOGIN</Link>
+                    <NavLink to="/Login" 
+                        className={({isActive}) => (isActive ? `${style.item} ${style.active}` : style.item)}>
+                        LOGIN
+                    </NavLink>
                 </li>
             </ul>
         </nav>
